72ed8fddf71b33a10e51ebb47ab8d067e18bddbc
James Ockhuis <james.o@adendorffs.co.za>
2023-04-28 16:17:46 +0200

    AB#12 Logging setup

---
+++ utils/logging.py
@@ -0,0 +1,16 @@
+from logging import config, getLogger
+import yaml
+
+
+def get_logger(logger_name: str = "elevate_trader"):
+    "Return the app logger"
+    return getLogger(logger_name)
+
+def file_logger_init():
+    "Initialize the logger from logging.yaml config file"
+    with open("logging.yaml", "r") as f:
+        yaml_content = yaml.safe_load(f.read())
+        config.dictConfig(yaml_content)
+    
+    return get_logger()
+
